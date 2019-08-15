f3d07cea7b847c553462d1f4d16611dee4607d17
tenuxi <jaaltonen86@gmail.com>
2019-08-15 09:42:42 +0300

    eka uppaus

---
+++ kolmas.php
@@ -0,0 +1,16 @@
+<div class="card mb-3">
+  <img src="img/test.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Kortti 1</h5>
+    <p class="card-text">Tää on kortti.</p>
+    <p class="card-text"><small class="text-muted">Last updated 2 mins ago</small></p>
+  </div>
+</div>
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Kortti 2</h5>
+    <p class="card-text">Tää nyt sitte on vaan kortti 2</p>
+    <p class="card-text"><small class="text-muted">Last updated 46 mins ago</small></p>
+  </div>
+  <img src="img/test.png" class="card-img-top" alt="...">
+</div>
